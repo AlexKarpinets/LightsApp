ff454e389a3e39cbcaaf7f14793715a7a1fdbb53
AlexKarpinets <66379289+AlexKarpinets@users.noreply.github.com>
2021-07-07 22:19:21 +0300

    Changes viewcontroller

--- LightsApp/ViewController.swift
+++ LightsApp/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstSquare: UIView!
+    @IBOutlet weak var secondSquare: UIView!
+    @IBOutlet weak var threeSquare: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        firstSquare.layer.cornerRadius = 50
+        secondSquare.layer.cornerRadius = 50
+        threeSquare.layer.cornerRadius = 50
+        startButton.layer.cornerRadius = 30
     }
-
-
+    
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("Next", for: .normal)
+        
+        if firstSquare.alpha == secondSquare.alpha {
+            firstSquare.alpha = 1
+            threeSquare.alpha = 0.3
+        } else if firstSquare.alpha > 0.3 &&
+                    secondSquare.alpha == threeSquare.alpha {
+            secondSquare.alpha = 1
+            firstSquare.alpha = 0.3
+        } else if secondSquare.alpha > 0.3 &&
+                    threeSquare.alpha == firstSquare.alpha {
+            threeSquare.alpha = 1
+            secondSquare.alpha = 0.3
+        }
+    }
 }
-
